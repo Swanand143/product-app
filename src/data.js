409e8c8ca232fceaeddd5e5c5a5207f0ee82455d
Swanand@143 <swanandgholap375@gmail.com>
2024-07-15 23:07:29 +0530

    description added

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const products = [
     price: "₹14,999",
     description:
       "Samsung Galaxy F23 5G with 6GB RAM, 128GB Storage, and Snapdragon 750G Processor.",
-    imageUrl: "https://example.com/images/samsung-galaxy-f23.jpg",
+    imageUrl: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/o/p/z/-original-imagcg22pf79cgau.jpeg?q=70",
     info : {
         part1 : "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
         part2: "16.76 cm (6.6 inch) Full HD+ Display",
@@ -20,7 +20,14 @@ const products = [
     price: "₹12,999",
     description:
       "vivo T2x 5G with 4GB RAM, 64GB Storage, and MediaTek Dimensity 700 Processor.",
-    imageUrl: "https://example.com/images/vivo-t2x.jpg",
+    imageUrl: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/4/h/e/-original-imagzjhwmsamexfk.jpeg?q=70",
+    info : {
+      part1 : "6 GB RAM | 64 GB ROM | Expandable Upto 1",
+      part2: "16.76 cm (6.6 inch) Full HD+ Display",
+      part3: "50MP + 2MP | 8MP Front Camera",
+      part4: "5000 mAh Lithium Ion Battery",
+      part5: "MediaTek Dimensity 700 Processor",
+      }
   },
   {
     id: "POCO M4 5G",
@@ -28,7 +35,14 @@ const products = [
     price: "₹10,999",
     description:
       "POCO M4 5G with 6GB RAM, 128GB Storage, and MediaTek Dimensity 700 Processor.",
-    imageUrl: "https://example.com/images/poco-m4.jpg",
+    imageUrl: "https://rukminim2.flixcart.com/image/312/312/l2jcccw0/mobile/i/p/o/-original-imagdu8j9vxscfdh.jpeg?q=70",
+    info : {
+      part1 : "6 GB RAM | 128 GB ROM | Expandable Upto 1",
+      part2: "16.76 cm (6.58 inch) Full HD+ Display",
+      part3: "50MP + 2MP | 8MP Front Camera",
+      part4: "5000 mAh Lithium Ion Battery",
+      part5: "MediaTek Dimensity 700 Processor",
+      }
   },
   {
     id: "OPPO K10 5G",
@@ -36,7 +50,7 @@ const products = [
     price: "₹15,999",
     description:
       "OPPO K10 5G with 8GB RAM, 128GB Storage, and MediaTek Dimensity 810 Processor.",
-    imageUrl: "https://example.com/images/oppo-k10.jpg",
+    imageUrl: "https://rukminim2.flixcart.com/image/312/312/l2jcccw0/mobile/h/x/3/-original-imagduwqakhhkrse.jpeg?q=70",
   },
   {
     id: "realme 9 Pro+ 5G",
--- src/view/productview/proview.js
+++ src/view/productview/proview.js
@@ -7,32 +7,36 @@ function BlogView() {
 
   const viewProducts = products.find((blogObject) => blogObject.id === id);
   return (
-    <div className="View-cards">
+    <div className="View-cards-container">
       <h1>{viewProducts.name}</h1>
-      <div className="view-image">
-        <img src={viewProducts.imageUrl} alt={viewProducts.name} />
-      </div>
-      <div className="View-price-container">
-        <p className="View-price">{viewProducts.price}</p>
-      </div>
-      <div className="View-content-container">
-        <ul>
-          <li>
-            <p className="View-content">{viewProducts.info.part1}</p>
-          </li>
-          <li>
-            <p className="View-content">{viewProducts.info.part2}</p>
-          </li>
-          <li>
-            <p className="View-content">{viewProducts.info.part3}</p>
-          </li>
-          <li>
-            <p className="View-content">{viewProducts.info.part4}</p>
-          </li>
-          <li>
-            <p className="View-content">{viewProducts.info.part5}</p>
-          </li>
-        </ul>
+      <div className="View-cards">
+        <div className="view-image">
+          <img src={viewProducts.imageUrl} alt={viewProducts.name} />
+          <div className="View-price-container">
+            <p className="View-price">{viewProducts.price}</p>
+          </div>
+        </div>
+
+        <div className="View-content-container">
+          <h3>Specifications :</h3>
+          <ul>
+            <li>
+              <p className="View-content">{viewProducts.info.part1}</p>
+            </li>
+            <li>
+              <p className="View-content">{viewProducts.info.part2}</p>
+            </li>
+            <li>
+              <p className="View-content">{viewProducts.info.part3}</p>
+            </li>
+            <li>
+              <p className="View-content">{viewProducts.info.part4}</p>
+            </li>
+            <li>
+              <p className="View-content">{viewProducts.info.part5}</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
